ce84bd0e1aeb10fc77acafcbe8e2a45f2db5a299
Nathandev024 <nathandev021@gmail.com>
2026-07-15 15:43:14 -0300

    modificação na  aba agendamento

--- frontend/src/pages/Agendamento.tsx
+++ frontend/src/pages/Agendamento.tsx
@@ -54,59 +54,83 @@ export default function Agendamento() {
           <div className={styles.calendarContainer}>
             <h1 className={styles.title}>Etapa 1 - Data e Horario</h1>
             <div className={styles.daysCard}>
-              <Calendar onChange={handleDateChange} value={date} />
+              <Calendar
+                className={styles.calendar}
+                onChange={handleDateChange}
+                value={date}
+              />
               <div className={styles.hoursCard}>
                 {AvailableTimes.map((Time) => (
-                  <button key={Time} onClick={() => setSelectedTime(Time)}>
+                  <button
+                    key={Time}
+                    onClick={() => setSelectedTime(Time)}
+                    className={selectedTime === Time ? styles.selectedHour : ""}
+                  >
                     {Time}
                   </button>
                 ))}
               </div>
             </div>
-            <button className={styles.btnSkip} onClick={() => setStep(2)}>
-              Avançar
-            </button>
+            <div className={styles.btnContainer}>
+              <button className={styles.btnSkip} onClick={() => setStep(2)}>
+                Avançar
+              </button>
+            </div>
           </div>
         )}
         {step === 2 && (
-          <div>
-            <h1 className={styles.title}>Etapa 2 - Serviço</h1>
-            {services.map((service) => (
-              <div
-                key={service.id}
-                onClick={() => toggleService(service.id)}
-                style={{
-                  border: serviceIds.includes(service.id)
-                    ? "2px solid gold"
-                    : "2px solid transparent",
-                }}
-              >
-                <button>{service.title}</button>
-                <button>{service.value}</button>
+          <div className={styles.card}>
+            <div className={styles.serviceContainer}>
+              <h1 className={styles.title}>Etapa 2 - Serviço</h1>
+              <div className={styles.btnCard}>
+                {services.map((service) => (
+                  <div
+                    key={service.id}
+                    onClick={() => toggleService(service.id)}
+                    style={{
+                      border: serviceIds.includes(service.id)
+                        ? "2px solid gold"
+                        : "2px solid transparent",
+                    }}
+                    className={styles.serviceCard}
+                  >
+                    <h3>{service.title}</h3>
+                    <span>R$ {service.value}</span>
+                  </div>
+                ))}
               </div>
-            ))}
-            <button onClick={() => setStep(1)}>Voltar</button>
-            <button onClick={() => setStep(3)}>Avançar</button>
+              <div className={styles.btnSkipEtapa2}>
+                <button onClick={() => setStep(1)}>Voltar</button>
+                <button onClick={() => setStep(3)}>Avançar</button>
+              </div>
+            </div>
           </div>
         )}
         {step === 3 && (
-          <div>
-            <h1 className={styles.title}>Etapa 3 - Confirmação</h1>
-            {selectedDate}
-            {selectedTime}
-            <button
-              onClick={async () => {
-                await createAgendamento({
-                  selectedDate,
-                  selectedTime,
-                  serviceId: serviceIds,
-                });
-                navigate("/meus-agendamentos");
-              }}
-            >
-              Confirmar
-            </button>
-            <button onClick={() => setStep(2)}>Voltar</button>
+          <div className={styles.etapa3Container}>
+            <div className={styles.etapa3Card}>
+              <h1 className={styles.title}>Etapa 3 - Confirmação</h1>
+              <div className={styles.subTitleCard}>
+                <p>
+                  DIA {selectedDate} AS {selectedTime}
+                </p>
+              </div>
+              <div className={styles.btnEtapa3}>
+                <button
+                  onClick={async () => {
+                    await createAgendamento({
+                      selectedDate,
+                      selectedTime,
+                      serviceId: serviceIds,
+                    });
+                    navigate("/meus-agendamentos");
+                  }}
+                >
+                  Confirmar
+                </button>
+                <button onClick={() => setStep(2)}>Voltar</button>
+              </div>
+            </div>
           </div>
         )}
       </div>
